190045ea0ed8fe71c958a834678fae4e1a277411
samuel <trsamual@gmail.com>
2024-11-01 18:14:06 +0530

    changed

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -6,6 +6,7 @@ import 'package:loginapp/widgets/login/login_btn.dart';
 class Loginscreen extends StatelessWidget {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+  final _formkEy = GlobalKey<FormState>();
   Loginscreen({
     super.key,
   });
@@ -25,32 +26,48 @@ class Loginscreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 45),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              style: const TextStyle(
-                fontFamily: 'second-super-font',
-                fontSize: 24,
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                style: const TextStyle(
+                  fontFamily: 'second-super-font',
+                  fontSize: 24,
+                ),
+                controller: usernameController,
+                decoration: inptFieldDecor(fieldTxt: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter Username";
+                  }
+                  return null;
+                },
               ),
-              controller: usernameController,
-              decoration: inptFieldDecor(fieldTxt: 'Username'),
-            ),
-            SizedBox(
-              height: size * 0.03,
-            ),
-            TextField(
-              obscureText: true,
-              controller: passwordController,
-              decoration: inptFieldDecor(fieldTxt: 'Password'),
-            ),
-            SizedBox(
-              height: size * 0.04,
-            ),
-            LoginBtn(
+              SizedBox(
+                height: size * 0.03,
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: inptFieldDecor(fieldTxt: 'Password'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter Password";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: size * 0.04,
+              ),
+              LoginBtn(
                 usernameController: usernameController,
-                passwordController: passwordController),
-          ],
+                passwordController: passwordController,
+                formKey: _formkEy,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/login/login_btn.dart
+++ lib/widgets/login/login_btn.dart
@@ -8,10 +8,12 @@ class LoginBtn extends StatelessWidget {
     super.key,
     required this.usernameController,
     required this.passwordController,
+    required this.formKey,
   });
 
   final TextEditingController usernameController;
   final TextEditingController passwordController;
+  final GlobalKey<FormState> formKey;
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +25,25 @@ class LoginBtn extends StatelessWidget {
         foregroundColor: secondaryColor,
       ),
       onPressed: () {
-        final result = login(
-          username: usernameController.text,
-          password: passwordController.text,
-        );
-        if (result == true) {
-          showSnackBar(
-            context: context,
-            text: 'Login success',
-            bgColor: const Color.fromARGB(255, 19, 90, 149),
+        if (formKey.currentState!.validate()) {
+          final result = login(
+            username: usernameController.text,
+            password: passwordController.text,
           );
-          Navigator.pushReplacementNamed(
-              context, "home-screen");
-        } else {
-          showSnackBar(
-            context: context,
-            text: 'Incorrect username or password',
-            bgColor: const Color.fromARGB(255, 149, 19, 19),
-          );
+          if (result == true) {
+            showSnackBar(
+              context: context,
+              text: 'Login success',
+              bgColor: const Color.fromARGB(255, 19, 90, 149),
+            );
+            Navigator.pushReplacementNamed(context, "home-screen");
+          } else {
+            showSnackBar(
+              context: context,
+              text: 'Incorrect username or password',
+              bgColor: const Color.fromARGB(255, 149, 19, 19),
+            );
+          }
         }
       },
       child: const Text("Login"),
